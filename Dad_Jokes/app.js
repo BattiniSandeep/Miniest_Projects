cf5cfec2bcd1be90be389900b61c8d53574c8053
BattiniSandeep <76659791+BattiniSandeep@users.noreply.github.com>
2021-12-14 20:13:11 +0530

    Added a Miniest Project.
    Added a tiny projects which displays jokes on click using API, Axios, JavaScript

---
+++ Dad_Jokes/app.js
@@ -0,0 +1,23 @@
+const jokes = document.querySelector('#jokesList');
+
+const button = document.querySelector('#jokesButton');
+
+const addNewJoke = async () => {
+    let joke = await getDadJoke();
+    const newLI = document.createElement('li');
+    newLI.append(joke);
+    jokes.append(newLI);
+}
+
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json' } }
+        const res = await axios.get('https://icanhazdadjoke.com/', config);
+        return res.data.joke;
+    } catch {
+        return "Sorry, No Jokes Available! :("
+    }
+
+}
+
+button.addEventListener('click', addNewJoke);
